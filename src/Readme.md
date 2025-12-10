24b9ecc9c626e6f9eb3761ea2df5d767774606ec
aakash <aakashprajapat2020@gmail.com>
2025-12-10 20:42:38 +0530

    angular llmdesk setup

---
+++ src/Readme.md
@@ -0,0 +1,8 @@
+## Docker Build Command
+```bash
+docker compose build --no-cache
+```
+## Docker Run Command
+```bash
+docker compose up
+```
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Agent } from './agent/agent';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path: 'agents',
+        component: Agent
+    }
+
+];
