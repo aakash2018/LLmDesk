086cf01ad67e295b058884d06c9081bbdfcd96b2
aakash <aakashprajapat2020@gmail.com>
2025-12-11 23:25:28 +0530

    dashboard and  agent page created

--- src/app/agent/agent.ts
+++ src/app/agent/agent.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../components/sidebar/sidebar';
+import { Header } from '../components/header/header';
+import { Breadcrumb } from '../components/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-agent',
-  imports: [],
+  imports: [Sidebar,Header,Breadcrumb],
   templateUrl: './agent.html',
   styleUrl: './agent.css',
 })
 export class Agent {
-
+isSidebarOpen: boolean = true;
 }
--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../components/sidebar/sidebar';
+import { Header } from '../components/header/header';
+import { Breadcrumb } from '../components/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Sidebar,Header,Breadcrumb],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
